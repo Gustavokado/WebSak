1f008a30905a197b1909367a745bd4c523f7f762
Gustavokado <54946763+Gustavokado@users.noreply.github.com>
2020-02-19 14:56:05 +0100

    Initial commit

---
+++ WebSak/Controllers/PokemonController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSak.Controllers
+{
+    [Route("api/pokemon")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        public static List<Pokemon> pokemons = new List<Pokemon>()
+        {
+            new Pokemon() {Name = "Ekans", Strength = 67},
+            new Pokemon() {Name = "Bulbasaur", Strength = 200},
+            new Pokemon() {Name = "Eva-lena", Strength = 999}
+        };
+
+
+        [HttpGet]
+        public ActionResult ExaktVadJagVill()
+        {
+            /*Pokemon p = new Pokemon();
+            p.Name = "Ekans";
+            p.Strength = 67;*/
+            return Ok(pokemons);
+        } 
+
+        [HttpPost]
+        public ActionResult AddNewPokemon(Pokemon newPokemon)
+        {
+            pokemons.Add(newPokemon);
+
+            return Created("", newPokemon);
+        }
+
+    }
+}
